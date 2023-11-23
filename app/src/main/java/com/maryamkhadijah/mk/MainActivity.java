52c22eb43557e6e1ee73cf3d0873f503bc3d5a7b
wanizainey <wanizainey@gmail.com>
2023-11-23 15:29:02 +0800

    Initial commit

---
+++ app/src/main/java/com/maryamkhadijah/mk/MainActivity.java
@@ -0,0 +1,68 @@
+package com.maryamkhadijah.mk;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignin, btnSignup;
+    private FirebaseAuth mAuth;
+    private EditText editTextEmail, editTextPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSignin = findViewById(R.id.btnLogin);
+        editTextEmail = findViewById(R.id.editEmailLogin);
+        editTextPassword = findViewById(R.id.editPasswordLogin);
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //perform login
+                String email, password;
+                email = editTextEmail.getText().toString();
+                password = editTextPassword.getText().toString();
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    Toast.makeText(MainActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                }else{
+
+                    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                //success
+                                String user_id = mAuth.getCurrentUser().getEmail();
+                                Toast.makeText(MainActivity.this, user_id, Toast.LENGTH_SHORT).show();
+                            }else {
+                                //fail
+
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+
+
+
+    }
+}
