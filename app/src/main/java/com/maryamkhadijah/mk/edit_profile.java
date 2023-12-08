141e196e0a7d2dc4889154e209ebf72e9f65a5e6
wanizainey <wanizainey@gmail.com>
2023-12-08 16:24:57 +0800

    edit profile done

--- app/src/main/java/com/maryamkhadijah/mk/edit_profile.java
+++ app/src/main/java/com/maryamkhadijah/mk/edit_profile.java
@@ -1,8 +1,18 @@
 package com.maryamkhadijah.mk;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class edit_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
 
 
 }
