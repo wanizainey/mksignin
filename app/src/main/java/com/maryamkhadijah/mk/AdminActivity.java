e37d2e10731df12ad71ddabc10aaa304acf1fc55
hakimi <hakimih653@gmail.com>
2023-12-28 16:04:17 +0800

    tambah calendar butang dan pAGE APPROVAL

--- app/src/main/java/com/maryamkhadijah/mk/AdminActivity.java
+++ app/src/main/java/com/maryamkhadijah/mk/AdminActivity.java
@@ -2,13 +2,29 @@ package com.maryamkhadijah.mk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminActivity extends AppCompatActivity {
+
+    private Button adminlistbtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+
+        adminlistbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminActivity.this, AdminApproval.class));
+            }
+        });
+
+
     }
 }
